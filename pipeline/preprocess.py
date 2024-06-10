1cd48a6dfd485078fc533018ae7cb425fedc6869
Artem <artichek123@gmail.com>
2024-06-10 23:08:31 +0300

    Add DVC pipeline

---
+++ pipeline/preprocess.py
@@ -0,0 +1,18 @@
+import click
+import pandas as pd
+from src.features.build_features import preprocess
+
+
+@click.command()
+@click.argument('input_filepath', type=click.Path(exists=True))
+@click.argument('output_filepath', type=click.Path())
+def main(input_filepath, output_filepath):
+    """Preprocess the raw data and save the processed data."""
+    df = pd.read_csv(input_filepath)
+    df_processed = preprocess(df)
+    df_processed.to_csv(output_filepath, index=False)
+    print(f"Processed data saved to {output_filepath}")
+
+
+if __name__ == '__main__':
+    main()
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -8,14 +8,17 @@ from src.features.build_features import preprocess
 
 def train_model(df_path):
     df = pd.read_csv(df_path)
-    df_train = preprocess(df)
+
+    df['HomePlanet'] = df['HomePlanet'].astype('category')
+    df['Destination'] = df['Destination'].astype('category')
+    df['Deck'] = df['Deck'].astype('category')
+    df['Side'] = df['Side'].astype('category')
 
     X_train, X_val, y_train, y_val = train_test_split(
-        df_train.drop('Transported', axis=1),
-        df_train['Transported'],
+        df.drop('Transported', axis=1),
+        df['Transported'],
         test_size=0.2, random_state=42
     )
-
     cat_features = np.where(X_train.dtypes == 'category')[0]
 
     best_params = {'iterations': 475,
@@ -29,14 +32,14 @@ def train_model(df_path):
                    'od_wait': 26}
 
     clf_model = CatBoostClassifier(iterations=best_params.get('iterations'),
-                             learning_rate=best_params.get('learning_rate'),
-                             depth=best_params.get('depth'),
-                             l2_leaf_reg=best_params.get('l2_leaf_reg'),
-                             bootstrap_type=best_params.get('bootstrap_type'),
-                             random_strength=best_params.get('random_strength'),
-                             bagging_temperature=best_params.get('bagging_temperature'),
-                             od_type=best_params.get('od_type'),
-                             od_wait=best_params.get('od_wait'))
+                                   learning_rate=best_params.get('learning_rate'),
+                                   depth=best_params.get('depth'),
+                                   l2_leaf_reg=best_params.get('l2_leaf_reg'),
+                                   bootstrap_type=best_params.get('bootstrap_type'),
+                                   random_strength=best_params.get('random_strength'),
+                                   bagging_temperature=best_params.get('bagging_temperature'),
+                                   od_type=best_params.get('od_type'),
+                                   od_wait=best_params.get('od_wait'))
 
     clf_model.fit(X_train, y_train, cat_features=cat_features, eval_set=(X_val, y_val))
 
@@ -44,4 +47,4 @@ def train_model(df_path):
 
 
 if __name__ == "__main__":
-    clf = train_model('../../data/raw/train.csv')
+    clf = train_model('../../data/processed/train_processed.csv')
